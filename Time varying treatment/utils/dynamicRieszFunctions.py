74783a305ded6d2d3d7a4fa4f563f0699096d6a2
Wisse Rutgers <152492223+wisse-rutgers@users.noreply.github.com>
2025-05-09 17:43:01 -0400

    Add files via upload

--- Time varying treatment/utils/dynamicRieszFunctions.py
+++ Time varying treatment/utils/dynamicRieszFunctions.py
@@ -6,12 +6,8 @@ import numpy as np
 from sklearn.model_selection import KFold
 import time
 
-lasso_cv_settings_global = {
-    'b_degree' : 1,
-    'cv_folds' : 5,
-    'random_state' : 42
-}
 lasso_a_settings_global = {
+    'estimate' : True,
     'lambda_val' : 0,
     'beta_start' : None,
     'D_LB' : 0,
@@ -24,6 +20,7 @@ lasso_a_settings_global = {
     'control' : {'maxIter': 1000, 'optTol': 1e-5, 'zeroThreshold': 1e-6}
 }
 lasso_f_settings_global = {
+    'estimate' : True,
     'lambda_val' : 0,
     'beta_start' : None,
     'D_LB' : 0,
@@ -36,6 +33,7 @@ lasso_f_settings_global = {
     'control' : {'maxIter': 1000, 'optTol': 1e-5, 'zeroThreshold': 1e-6}
 }
 rf_a_settings_global = {
+    'estimate' : True,
     'poly_degree' : 0,
     'l2' : 0,
     'n_estimators' : 100,
@@ -60,6 +58,7 @@ rf_a_settings_global = {
     'warm_start' : False
 }
 rf_f_settings_global = {
+    'estimate' : True,
     'poly_degree' : 1,
     'l2' : 0,
     'n_estimators' : 100,
@@ -84,6 +83,7 @@ rf_f_settings_global = {
     'warm_start' : False
 }
 net_a_settings_global = {
+    'estimate' : True,
     'test_split' : 0,
     'learner_lr' : 1e-4,
     'learner_l2' : 1e-3,
@@ -105,6 +105,7 @@ net_a_settings_global = {
     'act_func' : 'elu'
 }
 net_f_settings_global = {
+    'estimate' : True,
     'test_split' : 0,
     'learner_lr' : 1e-4,
     'learner_l2' : 1e-3,
@@ -131,7 +132,6 @@ class Trainer:
                   method_a = 'LASSO', method_f = "LASSO",
                     lasso_a_settings = lasso_a_settings_global,
                       lasso_f_settings = lasso_f_settings_global,
-                        lasso_cv_settings = lasso_cv_settings_global,
                             rf_a_settings = rf_a_settings_global,
                                 rf_f_settings = rf_f_settings_global,
                                     net_a_settings = net_a_settings_global,
@@ -157,8 +157,6 @@ class Trainer:
         # initalise trainers for f functions
         if method_f == 'LASSO':
             self.learners_f = [utils.dynamicRieszLASSO.Learner_f_LASSO(lasso_f_settings = lasso_f_settings) for _ in range(self.T)]
-        if method_f == 'LASSO_CV':
-            self.learners_f = [utils.dynamicRieszLASSO.Learner_f_LASSO_cv(lasso_cv_settings = lasso_cv_settings) for _ in range(self.T)]
         if method_f == 'RF':
             self.learners_f = [utils.dynamicRieszRF.Learner_f_RF(rf_f_settings = rf_f_settings) for _ in range(self.T)]
         if method_f == 'Net':
@@ -229,7 +227,6 @@ def estimateDynamicRiesz(Y, X, D, d, X_index, folds,
                           method_a = "LASSO", method_f = "LASSO",
                             lasso_a_settings = lasso_a_settings_global,
                                 lasso_f_settings = lasso_f_settings_global,
-                                    lasso_cv_settings = lasso_cv_settings_global,
                                         rf_a_settings = rf_a_settings_global,
                                             rf_f_settings = rf_f_settings_global,
                                                 net_a_settings = net_a_settings_global,
@@ -256,7 +253,6 @@ def estimateDynamicRiesz(Y, X, D, d, X_index, folds,
                           method_a, method_f,
                             lasso_a_settings = lasso_a_settings,
                                 lasso_f_settings = lasso_f_settings,
-                                    lasso_cv_settings = lasso_cv_settings,
                                         rf_a_settings = rf_a_settings,
                                             rf_f_settings = rf_f_settings,
                                                 net_a_settings = net_a_settings,
@@ -272,6 +268,8 @@ def estimateDynamicRiesz(Y, X, D, d, X_index, folds,
     
     return point, sigma, RR1, RR2, f1, f2
 
+
+# Functions if we want to estimate and combine the three different methods:
 class Trainer_all: 
     def __init__(self, Y, X, D, d, X_index,
                     lasso_a_settings = lasso_a_settings_global,
@@ -291,6 +289,14 @@ class Trainer_all:
         
         X_index (T,1) : Shows the index where each new set of covariates starts. E.g. (X1,X1,X1,X2,X2,X3,X3,X3) -> [0,3,5]. 
         """
+
+        self.estimate_lasso_a = lasso_a_settings['estimate']
+        self.estimate_lasso_f = lasso_f_settings['estimate']
+        self.estimate_rf_a = rf_a_settings['estimate']
+        self.estimate_rf_f = rf_f_settings['estimate']
+        self.estimate_net_a = net_a_settings['estimate']
+        self.estimate_net_f = net_f_settings['estimate']
+
         self.Y = Y
         self.X = X
         self.D = D
@@ -303,87 +309,78 @@ class Trainer_all:
         self.learners_f_RF = [utils.dynamicRieszRF.Learner_f_RF(rf_f_settings = rf_f_settings) for _ in range(self.T)]
         self.learners_f_Net = [utils.dynamicRieszNet.Learner_f_Net(net_f_settings=net_f_settings) for _ in range(self.T)]
 
-        rf_f_settings0 = rf_f_settings_global.copy()
-        rf_f_settings0['poly_degree'] = 0
-        self.learners_f_RF0 = [utils.dynamicRieszRF.Learner_f_RF(rf_f_settings = rf_f_settings0) for _ in range(self.T)]
-
         # initalise trainers for a functions
         self.learners_a_LASSO = [utils.dynamicRieszLASSO.Learner_a_LASSO(lasso_a_settings = lasso_a_settings) for _ in range(self.T)]
         self.learners_a_RF = [utils.dynamicRieszRF.Learner_a_RF(rf_a_settings = rf_a_settings) for _ in range(self.T)]
         self.learners_a_Net = [utils.dynamicRieszNet.Learner_a_Net(net_a_settings = net_a_settings) for _ in range(self.T)]
 
-        rf_a_settings1 = rf_a_settings_global.copy()
-        rf_a_settings1['poly_degree'] = 1
-        self.learners_a_RF1 = [utils.dynamicRieszRF.Learner_a_RF(rf_a_settings = rf_a_settings1) for _ in range(self.T)]
-
     def train(self):
         # Loop backwards over f_t functions
-        # for t in range(self.T-1,-1,-1):
-        #     if t == self.T-1:
-        #         f_hat = self.Y
-        #     else:
-        #         f_hat = self.learners_f_LASSO[t+1].predict(self.X[:, :(self.X_index[t+1]+1)], self.d[:, :(t+1+1)])
-        #     self.learners_f_LASSO[t].fit(f_hat, self.X[:, :(self.X_index[t]+1)], self.D[:, :(t+1)])
-        for t in range(self.T-1,-1,-1):
-            if t == self.T-1:
-                f_hat = self.Y
-            else:
-                f_hat = self.learners_f_RF[t+1].predict(self.X[:, :(self.X_index[t+1]+1)], self.d[:, :(t+1+1)])
-            self.learners_f_RF[t].fit(f_hat, self.X[:, :(self.X_index[t]+1)], self.D[:, :(t+1)])
-        # for t in range(self.T-1,-1,-1):
-        #     if t == self.T-1:
-        #         f_hat = self.Y
-        #     else:
-        #         f_hat = self.learners_f_Net[t+1].predict(self.X[:, :(self.X_index[t+1]+1)], self.d[:, :(t+1+1)])
-        #     self.learners_f_Net[t].fit(f_hat, self.X[:, :(self.X_index[t]+1)], self.D[:, :(t+1)])
-        # for t in range(self.T-1,-1,-1):
-        #     if t == self.T-1:
-        #         f_hat = self.Y
-        #     else:
-        #         f_hat = self.learners_f_RF0[t+1].predict(self.X[:, :(self.X_index[t+1]+1)], self.d[:, :(t+1+1)])
-        #     self.learners_f_RF0[t].fit(f_hat, self.X[:, :(self.X_index[t]+1)], self.D[:, :(t+1)])
+        if self.estimate_lasso_f:
+            for t in range(self.T-1,-1,-1):
+                if t == self.T-1:
+                    f_hat = self.Y
+                else:
+                    f_hat = self.learners_f_LASSO[t+1].predict(self.X[:, :(self.X_index[t+1]+1)], self.d[:, :(t+1+1)])
+                self.learners_f_LASSO[t].fit(f_hat, self.X[:, :(self.X_index[t]+1)], self.D[:, :(t+1)])
+        if self.estimate_rf_f:
+            for t in range(self.T-1,-1,-1):
+                if t == self.T-1:
+                    f_hat = self.Y
+                else:
+                    f_hat = self.learners_f_RF[t+1].predict(self.X[:, :(self.X_index[t+1]+1)], self.d[:, :(t+1+1)])
+                self.learners_f_RF[t].fit(f_hat, self.X[:, :(self.X_index[t]+1)], self.D[:, :(t+1)])
+        if self.estimate_net_f:
+            for t in range(self.T-1,-1,-1):
+                if t == self.T-1:
+                    f_hat = self.Y
+                else:
+                    f_hat = self.learners_f_Net[t+1].predict(self.X[:, :(self.X_index[t+1]+1)], self.d[:, :(t+1+1)])
+                self.learners_f_Net[t].fit(f_hat, self.X[:, :(self.X_index[t]+1)], self.D[:, :(t+1)])
             
         # Loop forwards over a_t functions
-        # for t in range(self.T):
-        #     if t == 0:
-        #         a_prev = torch.ones(self.Y.shape)
-        #     else:
-        #         a_prev = self.learners_a_LASSO[t-1].predict(self.X[:, :(self.X_index[t-1]+1)], self.D[:, :t])
-        #     self.learners_a_LASSO[t].fit(self.X[:, :(self.X_index[t]+1)], self.D[:, :(t+1)], self.d[:, :(t+1)], a_prev)
-        for t in range(self.T):
-            if t == 0:
-                a_prev = torch.ones(self.Y.shape)
-            else:
-                a_prev = self.learners_a_RF[t-1].predict(self.X[:, :(self.X_index[t-1]+1)], self.D[:, :t])
-            self.learners_a_RF[t].fit(self.X[:, :(self.X_index[t]+1)], self.D[:, :(t+1)], self.d[:, :(t+1)], a_prev)
-        # for t in range(self.T):
-        #     if t == 0:
-        #         a_prev = torch.ones(self.Y.shape)
-        #     else:
-        #         a_prev = self.learners_a_Net[t-1].predict(self.X[:, :(self.X_index[t-1]+1)], self.D[:, :t])
-        #     self.learners_a_Net[t].fit(self.X[:, :(self.X_index[t]+1)], self.D[:, :(t+1)], self.d[:, :(t+1)], a_prev)
-        # for t in range(self.T):
-        #     if t == 0:
-        #         a_prev = torch.ones(self.Y.shape)
-        #     else:
-        #         a_prev = self.learners_a_RF1[t-1].predict(self.X[:, :(self.X_index[t-1]+1)], self.D[:, :t])
-        #     self.learners_a_RF1[t].fit(self.X[:, :(self.X_index[t]+1)], self.D[:, :(t+1)], self.d[:, :(t+1)], a_prev)
+        if self.estimate_lasso_a:
+            for t in range(self.T):
+                if t == 0:
+                    a_prev = torch.ones(self.Y.shape)
+                else:
+                    a_prev = self.learners_a_LASSO[t-1].predict(self.X[:, :(self.X_index[t-1]+1)], self.D[:, :t])
+                self.learners_a_LASSO[t].fit(self.X[:, :(self.X_index[t]+1)], self.D[:, :(t+1)], self.d[:, :(t+1)], a_prev)
+        if self.estimate_rf_a:
+            for t in range(self.T):
+                if t == 0:
+                    a_prev = torch.ones(self.Y.shape)
+                else:
+                    a_prev = self.learners_a_RF[t-1].predict(self.X[:, :(self.X_index[t-1]+1)], self.D[:, :t])
+                self.learners_a_RF[t].fit(self.X[:, :(self.X_index[t]+1)], self.D[:, :(t+1)], self.d[:, :(t+1)], a_prev)
+        if self.estimate_net_a:
+            for t in range(self.T):
+                if t == 0:
+                    a_prev = torch.ones(self.Y.shape)
+                else:
+                    a_prev = self.learners_a_Net[t-1].predict(self.X[:, :(self.X_index[t-1]+1)], self.D[:, :t])
+                self.learners_a_Net[t].fit(self.X[:, :(self.X_index[t]+1)], self.D[:, :(t+1)], self.d[:, :(t+1)], a_prev)
 
 def estimateDynamicRiesz_all(Y, X, D, d, X_index, folds,
-
                             lasso_a_settings = lasso_a_settings_global,
                                 lasso_f_settings = lasso_f_settings_global,
-                                    lasso_cv_settings = lasso_cv_settings_global,
                                         rf_a_settings = rf_a_settings_global,
                                             rf_f_settings = rf_f_settings_global,
                                                 net_a_settings = net_a_settings_global,
                                                     net_f_settings = net_f_settings_global):
         
-    fold_results = torch.zeros(Y.shape[0],16)
-    RR1 = torch.zeros(Y.shape[0],16)
-    RR2 = torch.zeros(Y.shape[0],16)
-    f1 = torch.zeros(Y.shape[0],16)
-    f2 = torch.zeros(Y.shape[0],16)
+    estimate_lasso_a = lasso_a_settings['estimate']
+    estimate_lasso_f = lasso_f_settings['estimate']
+    estimate_rf_a = rf_a_settings['estimate']
+    estimate_rf_f = rf_f_settings['estimate']
+    estimate_net_a = net_a_settings['estimate']
+    estimate_net_f = net_f_settings['estimate']
+
+    fold_results = torch.zeros(Y.shape[0],9)
+    RR1 = torch.zeros(Y.shape[0],9)
+    RR2 = torch.zeros(Y.shape[0],9)
+    f1 = torch.zeros(Y.shape[0],9)
+    f2 = torch.zeros(Y.shape[0],9)
 
     # Iterate through folds
     kf = KFold(n_splits=folds, shuffle=True, random_state=42)
@@ -396,26 +393,34 @@ def estimateDynamicRiesz_all(Y, X, D, d, X_index, folds,
         Y_train, Y_test = Y[train_index], Y[test_index]
 
         # Train data for fold
-        trainer = Trainer_all(Y_train, X_train, D_train, d_train, X_index)
+        trainer = Trainer_all(Y_train, X_train, D_train, d_train, X_index, 
+                            lasso_a_settings = lasso_a_settings,
+                                lasso_f_settings = lasso_f_settings,
+                                        rf_a_settings = rf_a_settings,
+                                            rf_f_settings = rf_f_settings,
+                                                net_a_settings = net_a_settings,
+                                                    net_f_settings = net_f_settings)
         trainer.train()
 
         # Predict theta for every observation in the test data
-        # fold_results[test_index,:1], RR1[test_index,:1], RR2[test_index,:1], f1[test_index,:1], f2[test_index,:1] = thetahat(trainer.learners_f_LASSO, trainer.learners_a_LASSO, Y_test, X_test, D_test, d_test, X_index)   
-        # fold_results[test_index,1:2], RR1[test_index,1:2], RR2[test_index,1:2], f1[test_index,1:2], f2[test_index,1:2] = thetahat(trainer.learners_f_LASSO, trainer.learners_a_RF, Y_test, X_test, D_test, d_test, X_index)   
-        # fold_results[test_index,2:3], RR1[test_index,2:3], RR2[test_index,2:3], f1[test_index,2:3], f2[test_index,2:3] = thetahat(trainer.learners_f_LASSO, trainer.learners_a_Net, Y_test, X_test, D_test, d_test, X_index)   
-        # fold_results[test_index,3:4], RR1[test_index,3:4], RR2[test_index,3:4], f1[test_index,3:4], f2[test_index,3:4] = thetahat(trainer.learners_f_LASSO, trainer.learners_a_RF1, Y_test, X_test, D_test, d_test, X_index)   
-        # fold_results[test_index,4:5], RR1[test_index,4:5], RR2[test_index,4:5], f1[test_index,4:5], f2[test_index,4:5] = thetahat(trainer.learners_f_RF, trainer.learners_a_LASSO, Y_test, X_test, D_test, d_test, X_index)   
-        fold_results[test_index,5:6], RR1[test_index,5:6], RR2[test_index,5:6], f1[test_index,5:6], f2[test_index,5:6] = thetahat(trainer.learners_f_RF, trainer.learners_a_RF, Y_test, X_test, D_test, d_test, X_index)   
-        # fold_results[test_index,6:7], RR1[test_index,6:7], RR2[test_index,6:7], f1[test_index,6:7], f2[test_index,6:7] = thetahat(trainer.learners_f_RF, trainer.learners_a_Net, Y_test, X_test, D_test, d_test, X_index)   
-        # fold_results[test_index,7:8], RR1[test_index,7:8], RR2[test_index,7:8], f1[test_index,7:8], f2[test_index,7:8] = thetahat(trainer.learners_f_RF, trainer.learners_a_RF1, Y_test, X_test, D_test, d_test, X_index)   
-        # fold_results[test_index,8:9], RR1[test_index,8:9], RR2[test_index,8:9], f1[test_index,8:9], f2[test_index,8:9] = thetahat(trainer.learners_f_Net, trainer.learners_a_LASSO, Y_test, X_test, D_test, d_test, X_index)   
-        # fold_results[test_index,9:10], RR1[test_index,9:10], RR2[test_index,9:10], f1[test_index,9:10], f2[test_index,9:10] = thetahat(trainer.learners_f_Net, trainer.learners_a_RF, Y_test, X_test, D_test, d_test, X_index)   
-        # fold_results[test_index,10:11], RR1[test_index,10:11], RR2[test_index,10:11], f1[test_index,10:11], f2[test_index,10:11] = thetahat(trainer.learners_f_Net, trainer.learners_a_Net, Y_test, X_test, D_test, d_test, X_index)   
-        # fold_results[test_index,11:12], RR1[test_index,11:12], RR2[test_index,11:12], f1[test_index,11:12], f2[test_index,11:12] = thetahat(trainer.learners_f_Net, trainer.learners_a_RF1, Y_test, X_test, D_test, d_test, X_index)   
-        # fold_results[test_index,12:13], RR1[test_index,12:13], RR2[test_index,12:13], f1[test_index,12:13], f2[test_index,12:13] = thetahat(trainer.learners_f_RF0, trainer.learners_a_LASSO, Y_test, X_test, D_test, d_test, X_index)   
-        # fold_results[test_index,13:14], RR1[test_index,13:14], RR2[test_index,13:14], f1[test_index,13:14], f2[test_index,13:14] = thetahat(trainer.learners_f_RF0, trainer.learners_a_RF, Y_test, X_test, D_test, d_test, X_index)   
-        # fold_results[test_index,14:15], RR1[test_index,14:15], RR2[test_index,14:15], f1[test_index,14:15], f2[test_index,14:15] = thetahat(trainer.learners_f_RF0, trainer.learners_a_Net, Y_test, X_test, D_test, d_test, X_index)   
-        # fold_results[test_index,15:16], RR1[test_index,15:16], RR2[test_index,15:16], f1[test_index,15:16], f2[test_index,15:16] = thetahat(trainer.learners_f_RF0, trainer.learners_a_RF1, Y_test, X_test, D_test, d_test, X_index)   
+        if estimate_lasso_a & estimate_lasso_f:
+            fold_results[test_index,:1], RR1[test_index,:1], RR2[test_index,:1], f1[test_index,:1], f2[test_index,:1] = thetahat(trainer.learners_f_LASSO, trainer.learners_a_LASSO, Y_test, X_test, D_test, d_test, X_index)   
+        if estimate_rf_a & estimate_lasso_f:
+            fold_results[test_index,1:2], RR1[test_index,1:2], RR2[test_index,1:2], f1[test_index,1:2], f2[test_index,1:2] = thetahat(trainer.learners_f_LASSO, trainer.learners_a_RF, Y_test, X_test, D_test, d_test, X_index)   
+        if estimate_net_a & estimate_lasso_f:
+            fold_results[test_index,2:3], RR1[test_index,2:3], RR2[test_index,2:3], f1[test_index,2:3], f2[test_index,2:3] = thetahat(trainer.learners_f_LASSO, trainer.learners_a_Net, Y_test, X_test, D_test, d_test, X_index)   
+        if estimate_lasso_a & estimate_rf_f:
+            fold_results[test_index,3:4], RR1[test_index,3:4], RR2[test_index,3:4], f1[test_index,3:4], f2[test_index,3:4] = thetahat(trainer.learners_f_RF, trainer.learners_a_LASSO, Y_test, X_test, D_test, d_test, X_index)   
+        if estimate_rf_a & estimate_rf_f:
+            fold_results[test_index,4:5], RR1[test_index,4:5], RR2[test_index,4:5], f1[test_index,4:5], f2[test_index,4:5] = thetahat(trainer.learners_f_RF, trainer.learners_a_RF, Y_test, X_test, D_test, d_test, X_index)   
+        if estimate_net_a & estimate_rf_f:
+            fold_results[test_index,5:6], RR1[test_index,5:6], RR2[test_index,5:6], f1[test_index,5:6], f2[test_index,5:6] = thetahat(trainer.learners_f_RF, trainer.learners_a_Net, Y_test, X_test, D_test, d_test, X_index)   
+        if estimate_lasso_a & estimate_net_f:
+            fold_results[test_index,6:7], RR1[test_index,6:7], RR2[test_index,6:7], f1[test_index,6:7], f2[test_index,6:7] = thetahat(trainer.learners_f_Net, trainer.learners_a_LASSO, Y_test, X_test, D_test, d_test, X_index)   
+        if estimate_rf_a & estimate_net_f:
+            fold_results[test_index,7:8], RR1[test_index,7:8], RR2[test_index,7:8], f1[test_index,7:8], f2[test_index,7:8] = thetahat(trainer.learners_f_Net, trainer.learners_a_RF, Y_test, X_test, D_test, d_test, X_index)   
+        if estimate_net_a & estimate_net_f:
+            fold_results[test_index,8:9], RR1[test_index,8:9], RR2[test_index,8:9], f1[test_index,8:9], f2[test_index,8:9] = thetahat(trainer.learners_f_Net, trainer.learners_a_Net, Y_test, X_test, D_test, d_test, X_index)   
 
     point = torch.mean(fold_results,0)
     sigma2 = torch.mean( (fold_results - point) ** 2 ,0)
