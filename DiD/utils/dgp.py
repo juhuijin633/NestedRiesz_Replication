f188e78c574db9fdaea0bae55ea3989b523bd2ca
Marvin Lob <malo4300@outlook.de>
2025-05-27 09:58:21 -0400

    added ATT simulation

--- DiD/utils/dgp.py
+++ DiD/utils/dgp.py
@@ -35,14 +35,36 @@ class DiD_DGP:
         X2 = X1 + torch.ones(n, self.dim_X) *  (D *(1 + eta)).unsqueeze(1)  + eps_x
         X21 = X2[:, 0]
         Y2 = Y1 + self.c_1 * D.squeeze() + self.beta_2 * (X21 > 0).float() + self.delta_3* D.squeeze() * X21 + torch.randn(n)
-
-        self.ATT = torch.mean(Y2[D.squeeze() == 1] - Y1[D.squeeze() == 1])
         return {
             "X1": X1,
             "X2": X2,
-            "D": D.squeeze(),
-            "Z": Z,
             "Y1": Y1,
             "Y2": Y2,
-            "ATT": self.ATT,
-        }
+            "D": D,
+            "Z": Z,
+        }    
+    def simulate_ATT(self, n = 1000000):
+        X1 = torch.randn(n, self.dim_X)
+        X11 = X1[:, 0]
+        eta = torch.randn(n)
+        eps_x = torch.randn(n, self.dim_X)
+        Z = torch.randn(n, self.dim_Z)
+        Y1 = self.beta_1 * (X11 > 0).float() + self.delta_1 * X11 + Z @ self.gamma_2 + torch.randn(n) 
+        prob_D = self.g(self.delta_2* X11 + Z @ self.gamma_1 + self.alpha_1 * Y1 + eta) 
+        D = torch.bernoulli(prob_D)
+        X2_0 = X1 + eps_x
+        X2_1 = X1 + torch.ones(n, self.dim_X) *  (1 + eta).unsqueeze(1)  + eps_x
+        X21_0 = X2_0[:, 0]
+        X21_1 = X2_1[:, 0]
+        # Calculate ATT
+        E_X1D1 = torch.mean(X11[D == 1])  # E[X11 | D = 1]
+        E_ETAD1 = torch.mean(eta[D == 1]) # E[ETA | D = 1]
+        I_X21D1 = (X21_1 >0).float()
+        I_X20D1 = (X21_0 > 0).float()
+        E_X21D1 = torch.mean(I_X21D1[D == 1]) # P(X21(1) > 0 | D = 1)
+        E_X20D1 = torch.mean(I_X20D1[D == 1])  # P(X21(0) > 0 | D = 1)
+        ATT = self.c_1 + self.beta_2 * E_X21D1 - self.beta_2 * E_X20D1 + self.delta_3 * E_X1D1 + self.delta_3* E_ETAD1 + self.delta_3*1
+        return {"ATT": ATT, 
+                "E_X1D1": E_X1D1, "E_ETAD1": E_ETAD1, 
+                "E_X21D1": E_X21D1, "E_X20D1": E_X20D1,}
+                
